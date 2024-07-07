6d6e827677e6b620dcd737dbc7f80ba54afd4fb4
Alonso <alo.gomezc@gmail.com>
2024-07-07 12:38:16 -0400

    Integración de hover en buttons y colores en variables

--- js/index.js
+++ js/index.js
@@ -5,8 +5,8 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 function borrarTexto(elemento) {
-    console.log(elemento.value);
     if (elemento.value === "Ingrese algún texto") {
         elemento.value = "";
     }
 }
+
